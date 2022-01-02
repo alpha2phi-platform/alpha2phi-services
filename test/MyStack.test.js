fd47ddee177701e1e1e7fbc6bd9e80b4234324af
alpha2phi <alpha2phi@gmail.com>
2022-01-02 12:05:35 +0800

    Initial commit

---
+++ test/MyStack.test.js
@@ -0,0 +1,14 @@
+import { expect, haveResource } from "@aws-cdk/assert";
+import * as sst from "@serverless-stack/resources";
+import MyStack from "../stacks/MyStack";
+
+test("Test Stack", () => {
+  const app = new sst.App();
+  app.setDefaultFunctionProps({
+    runtime: "python3.7"
+  });
+  // WHEN
+  const stack = new MyStack(app, "test-stack");
+  // THEN
+  expect(stack).to(haveResource("AWS::Lambda::Function"));
+});
