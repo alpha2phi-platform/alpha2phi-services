900a9504bab578918ddec655a4d76d603985c1ff
alpha2phi <alpha2phi@gmail.com>
2022-01-02 22:59:07 +0800

    Initial commit

--- src/lambda.py
+++ src/lambda.py
@@ -1,7 +1,3 @@
 def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hello, World! Your request was received at {}.".format(
-            event["requestContext"]["time"]
-        ),
-    }
+    print("hello cron")
+    return {}
--- stacks/MyStack.js
+++ stacks/MyStack.js
@@ -4,19 +4,13 @@ export default class MyStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // Create a HTTP API
-    const api = new sst.Api(this, "Api", {
+    // Create Cron Job
+    new sst.Cron(this, "Cron", {
       defaultFunctionProps: {
         srcPath: "src",
       },
-      routes: {
-        "GET /": "lambda.handler",
-      }
-    });
-
-    // Show the endpoint in the output
-    this.addOutputs({
-      "ApiEndpoint": api.url,
+      schedule: "rate(1 minute)",
+      job: "lambda.handler",
     });
   }
 }
